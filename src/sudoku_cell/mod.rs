88205c209aef1c338db36486b097eca11217074e
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-02-09 16:49:37 +0200

    feat: cell macro

--- src/sudoku_cell/mod.rs
+++ src/sudoku_cell/mod.rs
@@ -5,6 +5,20 @@ use crate::sudoku_number::SudokuNumber;
 #[derive(Clone, Copy)]
 pub struct SudokuCell(pub Option<SudokuNumber>);
 
+#[macro_export]
+macro_rules! cell {
+    (1) => { Some(SudokuNumber::_1) };
+    (2) => { Some(SudokuNumber::_2) };
+    (3) => { Some(SudokuNumber::_3) };
+    (4) => { Some(SudokuNumber::_4) };
+    (5) => { Some(SudokuNumber::_5) };
+    (6) => { Some(SudokuNumber::_6) };
+    (7) => { Some(SudokuNumber::_7) };
+    (8) => { Some(SudokuNumber::_8) };
+    (9) => { Some(SudokuNumber::_9) };
+    () => { None };
+}
+
 impl Default for SudokuCell {
     fn default() -> Self {
         Self(None)
