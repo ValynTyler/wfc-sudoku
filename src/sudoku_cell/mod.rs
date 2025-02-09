b94c56dd9ef084644e17a3463ad9a61131a89836
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-02-09 18:04:09 +0200

    fix: empty spaces now print as periods

--- src/sudoku_cell/mod.rs
+++ src/sudoku_cell/mod.rs
@@ -34,7 +34,7 @@ impl Display for SudokuCell {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self.0 {
             Some(n) => write!(f, "{}", n),
-            None => write!(f, " "),
+            None => write!(f, "."),
         }
     }
 }
