77e6a1bc6e28a29ec0da8489df8566550b20e7fb
Valyn Tyler <115489989+ValynTyler@users.noreply.github.com>
2025-02-09 17:57:04 +0200

    feat: printing line repetitions

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,19 @@ use wfc_sudoku::{cell, sudoku_grid::SudokuGrid, sudoku_number::SudokuNumber};
 fn main() {
     let mut grid = SudokuGrid::default();
 
-    for i in 1..10 {
-        for j in 1..10 {
-            let num = 9 - (i + j) % 9;
-            grid.set_cell(j, i, cell!(num));
-        }
-    }
+    grid.set_cell(1, 0, cell!(1));
+    grid.set_cell(1, 1, cell!(5));
+    grid.set_cell(1, 2, cell!(1));
+    grid.set_cell(1, 5, cell!(5));
+    grid.set_cell(1, 7, cell!(9));
+    grid.set_cell(1, 8, cell!(9));
 
     println!("{}", grid);
+
+    if let Err(e) = grid.check_row(1) {
+        println!("Err: repeating numbers:");
+        for num in e {
+            println!("{}", num);
+        }
+    }
 }
